52090c21c25100f8f10585dcf96e5da4e8f11bd8
Kamariddin Sharapov <tadjibaev.ulugbek@gmail.com>
2020-01-24 00:33:02 +0500

    fix

--- helper/downloader.js
+++ helper/downloader.js
@@ -13,6 +13,8 @@ exports.downloadPhotos = function (links, path, done) {
             console.log('DOWNLOAD: '+url);
             const request = http.get(url, function (response) {
                 response.pipe(file);
+            }).then(function(){
+                console.log('DOWNLOAD: '+url);
             });
         }));
         Promise.all(downloadTasks).then(done);
