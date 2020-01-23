4484c9e90c3add54c7f86dcd611f0186c03ca339
Kamariddin Sharapov <tadjibaev.ulugbek@gmail.com>
2020-01-24 00:10:20 +0500

    fix

--- helper/downloader.js
+++ helper/downloader.js
@@ -7,8 +7,10 @@ var _ = require('underscore');
 exports.downloadPhotos = function (links, path, done) {
     console.log('../' + path + '/*');
     rimraf('../' + path + '/*', function () {
+        console.log('OLD FILES DELETED');
         const downloadTasks = _.values(links).map((url) => new Promise((resolve, reject) => {
             const file = fs.createWriteStream(path + '/' + url.split('/').pop());
+            console.log('DOWNLOAD: '+url);
             const request = http.get(url, function (response) {
                 response.pipe(file);
             });
