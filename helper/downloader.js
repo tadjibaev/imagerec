5317ebb911f6b9d1eed624a81e68f4171370917e
Kamariddin Sharapov <tadjibaev.ulugbek@gmail.com>
2020-01-23 20:00:08 +0500

    fix

--- helper/downloader.js
+++ helper/downloader.js
@@ -6,6 +6,8 @@ var _ = require('underscore');
 
 exports.downloadPhotos = function(links,path,done) {
     rimraf(path+'/*', function () {
+        console.log(links);
+        
         const downloadTasks = _.values(links).map((url) => new Promise((resolve, reject) => {
             const file = fs.createWriteStream(path+'/'+url.split('/').pop());
             console.log(url);
