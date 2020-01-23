2382ca46c886fc26a1da336bbae5afdfca558702
Kamariddin Sharapov <tadjibaev.ulugbek@gmail.com>
2020-01-23 23:35:57 +0500

    fix

--- helper/trainer.js
+++ helper/trainer.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const host = '52.213.135.19';
 exports.clearServer = function() {
     axios.post('http://' + host + ':9003/configuration/clear').then(response => {
         var status = response.data.status;
