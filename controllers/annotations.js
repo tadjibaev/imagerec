f348b5a4022a78b0a52048c07a04b893b7363a1f
Kamariddin Sharapov <tadjibaev.ulugbek@gmail.com>
2020-01-24 15:19:21 +0500

    fix

--- controllers/annotations.js
+++ controllers/annotations.js
@@ -3,6 +3,8 @@ exports.getAnnotations = function(supplier_id,next) {
     console.log('http://keysale.se/admin/recognition/dashboard/train?suplier_id='+supplier_id+'&data_type=annotations');
 	axios.post('http://keysale.se/admin/recognition/dashboard/train?suplier_id='+supplier_id+'&data_type=annotations').then(response => {
         var annotations = response.data;
+        console.log(annotations);
+        
         next(annotations);
     });
 }
